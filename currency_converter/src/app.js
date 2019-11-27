a006a17b7a6b30de2921471e1514374a7a295a64
MaryBobs <55357115+MaryBobs@users.noreply.github.com>
2019-11-27 08:26:41 +0000

    final commit

--- currency_converter/src/app.js
+++ currency_converter/src/app.js
@@ -6,20 +6,21 @@ document.addEventListener('DOMContentLoaded', () => {
     data: {
       initialValue: 0,
       exchangeRates: [],
-      selectedCurrency: null
+      selectedCurrency: null,
+      converted: 0
     },
     mounted(){
       this.fetchRates();
     },
+
   methods:  {
       fetchRates: function(){
         fetch("https://api.exchangeratesapi.io/latest")
         .then(response => response.json())
         .then(rates => this.exchangeRates = rates.rates)
      },
-     saveInitialValue: function(){
-       inputValue = this.initialValue
-       console.log(initialValue);
+     convertCurrency: function () {
+       this.converted = this.intialValue * this.selectedCurrency;
      }
    }
   })
